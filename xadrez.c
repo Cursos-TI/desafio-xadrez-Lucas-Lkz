70ee9ba87e2119c9ec450ef54dbb1e323210ba0b
Lucas-Lkz <202603326561@alunos.estacio.br>
2026-03-25 06:07:24 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -6,53 +6,69 @@ int main () {
     int MOV_TORRE = 5;
     int MOV_RAINHA = 8;
     int MOV_CAVALO = 8;
+    int MOV_PEAO = 2;
 
 
-    printf("Movimentação das peças de xadrez: \n");
-    printf("Movimento da torre: \n");
-    for (int i = 1; i <= MOV_TORRE; i++) {
-        printf("Direita\n");
-    }
-    
-    printf("\n---------------------------------\n");
-    printf("Movimentação do bispo: \n");
+    printf("Movimentação das peças de Xadrez\n");
+    printf("Movimentação da Rainha:\n");
 
-    int contadorBispo = 1;
-       
-        while (contadorBispo <= MOV_BISPO) { 
-        printf("Cima, direita\n");
-        contadorBispo++;
-       } 
-       //criei o contador bispo para o while parar, a cada vez que ele roda
-       //ele adiciona 1 ao contadorBispo, através do contadorBispo++;
-       // e quando ele for maior ou igual a variavel MOV_BISPO, ele vai parar
-    
-    printf("\n-------------------------\n");   
-    printf("Movimentação da rainha: \n");
     int contadorRainha = 1;
-    
-        do {
+    do
+    {
         printf("Esquerda\n");
         contadorRainha++;
-    }   while (contadorRainha <= MOV_RAINHA);
+    } while (contadorRainha <= MOV_RAINHA);
 
-    printf("\n---------------------------------\n");
-    printf("Movimentação do cavalo: \n");
-    int contadorCavalo = 1;
-        do {
-          if (contadorCavalo == 1) {
-             printf("Iniciando Salto: PRA CIMAAA!!!\n");
-          } else if (contadorCavalo ==8) {
-            printf("Pouso Final: CIMA!\n");
-          } else {
-            printf("Cima\n");
-          }
-          
+    printf("\n ---------------------------\n");
+    
+    printf("Movimentação do Bispo:\n");
+    for (int i = 1; i <= MOV_BISPO; i++)
+    {
+        printf("Cima, direita\n");
+    }
 
-        contadorCavalo++; //aqui faz o loop acontecer
-        } while (contadorCavalo <= MOV_CAVALO);
+    printf("\n ---------------------------\n");
+    printf("Movimentação da Torre:\n");
+int contadorTorre = 1;
+    while (contadorTorre <= MOV_TORRE)
+    {
+        printf("Cima\n");
+        contadorTorre++;
+    }
+
+    printf("\n ---------------------------\n");
+    printf("Movimentação do Cavalo:\n");
+
+    for (int i = 1; i <= MOV_CAVALO; i++)
+    {
+        switch (i)
+        {
+        case 1:
+            printf("Preparando pulo PRA CIMAAA!\n");
+            break;
         
+        case 8:
+            printf("POUSO!!\n");
+            break;
         
+        default:
+            printf("Cima!\n");
+            break;
+        }
+    }
 
-    return 0;
+    printf("\n ---------------------------\n");
+    printf("Movimentação do peão:\n");
+
+    for (int i = 1; i <= MOV_PEAO; i++)
+    {
+        printf("Baixo\n");
+    }
+    
+    
+    
+    
+    
+
+   return 0; 
 }
